7fb0c5a774f49466cdec4d1e31060d9534ccc421
tsuyopon-xyz <tsuyoshi4business+tsuyopon.xyz@gmail.com>
2019-08-09 22:44:25 +0900

    Created the answer code.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-container">
+      <h1 className="App-title">Hello, World!</h1>
+      <p>こんにちは世界！</p>
+    </div>
+  );
+}
+
+export default App;
